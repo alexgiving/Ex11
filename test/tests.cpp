fc873b6e83e06afb894c612988fc92c89d6060b0
alexgiving <alexgiving@mail.ru>
2021-04-05 18:17:26 +0300

    follow google style

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,44 +5,43 @@
 #include "train.h"
 
 TEST(Train, test_add_cage) {
-	size_t N = 10;
-	Train train;
-	for (size_t i = 0; i < N; ++i) {
-		Cage* cage = new Cage();
-		train.addCage(cage);
-	}
-	size_t expected = N;
-	size_t result = train.getLength();
-	EXPECT_EQ(expected, result);
+    Train train;
+    for (size_t i = 0; i < N; ++i) {
+        Cage* cage = new Cage();
+        train.addCage(cage);
+    }
+    size_t expected = N;
+    size_t result = train.getLength();
+    EXPECT_EQ(expected, result);
 }
 
 TEST(Train, test_) {
-	size_t N = 15;
-	Train train;
-	for (size_t i = 0; i < N; ++i) {
-		Cage* cage = new Cage();
-		train.addCage(cage);
-	}
-	size_t expected = train.checkLength();
-	size_t result = train.getLength();
-	EXPECT_EQ(expected, result);
+    size_t N = 15;
+    Train train;
+    for (size_t i = 0; i < N; ++i) {
+        Cage* cage = new Cage();
+        train.addCage(cage);
+    }
+    size_t expected = train.checkLength();
+    size_t result = train.getLength();
+    EXPECT_EQ(expected, result);
 }
 
 TEST(Train, test3) {
-	Train train;
-	size_t N = 15;
-	for (size_t i = 0; i < N; ++i) {
-		Cage* cage = new Cage();
-		train.addCage(cage);
-	}
-	size_t f1 = train.getLength();
-	size_t M = 10;
-	for (size_t i = 0; i < M; ++i) {
-		Cage* cage = new Cage();
-		train.addCage(cage);
-	}
-	size_t f2 = train.getLength();
-	size_t expected = N + M;
-	size_t result = f1 + f2;
-	EXPECT_EQ(expected, result);
+    Train train;
+    size_t N = 15;
+    for (size_t i = 0; i < N; ++i) {
+        Cage* cage = new Cage();
+        train.addCage(cage);
+    }
+    size_t f1 = train.getLength();
+    size_t M = 10;
+    for (size_t i = 0; i < M; ++i) {
+        Cage* cage = new Cage();
+        train.addCage(cage);
+    }
+    size_t f2 = train.getLength();
+    size_t expected = N + M;
+    size_t result = f1 + f2;
+    EXPECT_EQ(expected, result);
 }
